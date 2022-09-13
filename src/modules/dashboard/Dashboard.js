5933800dc298e9bd69a36edc48c993effdc9700a
Santosh <santosh@rumsan.com>
2022-09-14 03:11:13 +0545

    Align dashboard portlets fixes #31

--- src/modules/dashboard/Dashboard.js
+++ src/modules/dashboard/Dashboard.js
@@ -1,86 +1,42 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Row, Col } from 'reactstrap';
 import { useToasts } from 'react-toast-notifications';
-import TokenByProject from './tokens_by_project';
-import BeneficiaryByProject from './beneficiary_by_project';
+
 import { StatsCard } from '../ui_components/cards';
 import { TOAST } from '../../constants';
 import { UserContext } from '../../contexts/UserContext';
-import TransactionChart from '../ui_components/chart';
 import MultiSigTrigger from './MultiSigTrigger';
 import Map from '../../views/map/Map';
+
+import { getBeneficiariesSummary } from '../../services/stats';
+
+import BenByWard from './benByWard';
+import BenByGender from './benByGender';
+import BenByPhone from './benByPhone';
+import BenByBank from './benByBank';
 
 const Dashboard = () => {
 	const { addToast } = useToasts();
 
-	const { getDashboardStats } = useContext(UserContext);
 	const [stats, setStats] = useState({
-		totalProjects: 0,
-		totalVendors: 0,
-		totalBeneficiaries: 0,
-		totalMobilizers: 0,
-		totalAllocation: 0,
-		redeemedTokens: 0,
-		beneficiariesByProject: [],
-		tokensByProject: [],
-		totalInstitutions: 0
-	});
-	const [exportData, setExportData] = useState({
-		tokens_by_project: [],
-		benef_by_project: []
+		total_children: 0,
+		total_persons: 0,
+		total_beneficiaries: 0,
+		total_unbanked: 0,
+		total_tokens: 0,
+		total_redeem: 0
 	});
 
 	const fetchDashboardStats = () => {
-		getDashboardStats()
+		getBeneficiariesSummary()
 			.then(d => {
-				const {
-					projectCount,
-					vendorCount,
-					beneficiary,
-					mobilizerCount,
-					tokenAllocation,
-					institutionCount,
-					tokenRedemption
-				} = d;
-				if (beneficiary && beneficiary.project.length) setBeneficiaryByProjectExport(beneficiary.project);
-				if (tokenAllocation && tokenAllocation.projectAllocation.length)
-					setTokensByProjectExport(tokenAllocation.projectAllocation);
-				setStats(prevState => ({
-					...prevState,
-					totalProjects: projectCount,
-					totalVendors: vendorCount,
-					totalBeneficiaries: beneficiary.totalCount,
-					totalMobilizers: mobilizerCount,
-					totalAllocation: tokenAllocation.totalAllocation,
-					redeemedTokens: tokenRedemption.totalTokenRedemption,
-					beneficiariesByProject: beneficiary.project,
-					tokensByProject: tokenAllocation.projectAllocation,
-					totalInstitutions: institutionCount
-				}));
+				d.total_tokens = 0;
+				d.total_redeem = 0;
+				setStats(d);
 			})
 			.catch(() => {
 				addToast('Internal server error!', TOAST.ERROR);
 			});
-	};
-
-	const setBeneficiaryByProjectExport = data => {
-		const export_data = data.map(d => {
-			return { Project: d.name, Count: d.count };
-		});
-		setExportData(prevState => ({
-			...prevState,
-			benef_by_project: export_data
-		}));
-	};
-
-	const setTokensByProjectExport = data => {
-		const export_data = data.map(d => {
-			return { Project: d.name, Tokens: d.token };
-		});
-		setExportData(prevState => ({
-			...prevState,
-			tokens_by_project: export_data
-		}));
 	};
 
 	useEffect(fetchDashboardStats, []);
@@ -92,58 +48,64 @@ const Dashboard = () => {
 					<Row>
 						<Col md="4">
 							<StatsCard
-								title="Projects"
+								title="Beneficiaries"
+								subtitle="persons"
 								title_color="#2b7ec1"
 								icon_color="#2b7ec1"
 								icon_name="fas fa-clone"
-								data={stats.totalProjects}
+								data={stats.total_beneficiaries}
 							/>
 						</Col>
 						<Col md="4">
 							<StatsCard
-								title="Beneficiaries"
+								title="Children Under 5"
+								subtitle="children"
 								title_color="#80D5AA"
 								icon_color="#80D5AA"
 								icon_name="fas fa-users"
-								data={stats.totalBeneficiaries}
+								data={stats.total_children}
 							/>
 						</Col>
 						<Col md="4">
 							<StatsCard
-								title="Vendors"
+								title="Total Impact"
+								subtitle="persons"
 								title_color="#F49786"
 								icon_color="#F49786"
 								icon_name="fas fa-money-bill-alt"
-								data={stats.totalVendors}
+								data={stats.total_persons}
 							/>
 						</Col>
 					</Row>
 					<Row>
 						<Col md="4">
 							<StatsCard
-								title="Mobilizers"
+								title="Unbanked"
+								subtitle="persons"
 								title_color="#F49786"
 								icon_color="#F49786"
 								icon_name="fas fa-user-plus"
-								data={stats.totalMobilizers}
+								data={stats.total_unbanked}
 							/>
 						</Col>
 						<Col md="4">
 							<StatsCard
 								title="Token Issued"
+								subtitle="tokens"
 								title_color="#F7C087"
 								icon_color="#F7C087"
 								icon_name="fas fa-microchip"
-								data={stats.totalMobilizers}
+								data={stats.total_tokens}
 							/>
 						</Col>
 						<Col md="4">
 							<StatsCard
 								title="Token Redeem"
+								subtitle="tokens"
 								title_color="#80D5AA"
 								icon_color="#80D5AA"
 								icon_name="fas fa-dollar-sign"
-								data={stats.totalMobilizers}
+								data={stats.total_redeem}
 							/>
 						</Col>
 					</Row>
@@ -153,20 +115,22 @@ const Dashboard = () => {
 				</Col>
 			</Row>
 			<Row>
-				<Col md="7">
-					<TokenByProject data={stats.tokensByProject} exportData={exportData.tokens_by_project || []} />
-					<BeneficiaryByProject
-						releasedToken={stats.totalAllocation}
-						redeemedTokens={stats.redeemedTokens}
-						data={stats.beneficiariesByProject}
-						exportData={exportData.benef_by_project || []}
-					/>
+				<Col md="4">
+					<BenByGender />
 				</Col>
-				<Col md="5"></Col>
+				<Col md="4">
+					<BenByBank />
+				</Col>
+				<Col md="4">
+					<BenByPhone />
+				</Col>
 			</Row>
 			<Row>
-				<Col md="12">
-					<Map />
+				<Col md="6">
+					<BenByWard />
+				</Col>
+				<Col md="6">
+					<Map height="515px" />
 				</Col>
 			</Row>
 		</>
---
+++ src/modules/dashboard/benByWard.js
@@ -0,0 +1,78 @@
+import React, { useCallback, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Card, CardBody, CardTitle } from 'reactstrap';
+
+import { getBeneficiariesByWard } from '../../services/stats';
+
+const barChartData = {
+	datasets: [
+		{
+			label: '# of Beneficiaries',
+			backgroundColor: '#2B7EC1',
+			stack: '2'
+		}
+	]
+};
+
+const barChartOptions = {
+	maintainAspectRatio: false,
+	legend: {
+		display: true,
+		position: 'bottom'
+	},
+	scales: {
+		xAxes: [
+			{
+				gridLines: { display: false },
+				barThickness: 40
+			}
+		],
+		yAxes: [
+			{
+				gridLines: { borderDash: [4, 2] }
+			}
+		]
+	}
+};
+
+const Index = props => {
+	const getData = useCallback(async () => {
+		let bar_labels = [];
+		let bar_data = [];
+		let locData = await getBeneficiariesByWard();
+		if (locData && locData.length) {
+			for (let d of locData) {
+				bar_labels.push(d._id);
+				bar_data.push(d.count);
+			}
+		}
+
+		bar_labels.sort((a, b) => a - b);
+
+		barChartData.labels = bar_labels;
+		barChartData.datasets[0].data = bar_data;
+	}, []);
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div>
+			<Card>
+				<CardBody>
+					<div style={{ display: 'flex', justifyContent: 'space-between' }}>
+						<CardTitle>Beneficiaries by ward</CardTitle>
+					</div>
+					<br />
+					<div className="chart-wrapper" style={{ width: '100%', margin: '0 auto', height: 420 }}>
+						{/* <Bar data={barChartData} options={barChartOptions} /> */}
+						<Bar data={barChartData} options={barChartOptions} />
+					</div>
+				</CardBody>
+			</Card>
+		</div>
+	);
+};
+
+export default Index;
