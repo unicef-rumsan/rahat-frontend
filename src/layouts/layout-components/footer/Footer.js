15121be846e2c16ee02947fd3d58ba452e3bcc7d
Santosh <santosh@rumsan.com>
2022-09-16 13:10:54 +0545

    Check Manager ACL Fixes #45

--- src/layouts/layout-components/footer/Footer.js
+++ src/layouts/layout-components/footer/Footer.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
-  return (
-    <footer className="footer text-center">
-      All Rights Reserved by&nbsp;
-      <a target="_blank" rel="noopener noreferrer" href="https://rumsan.com">
-        Rumsan
-      </a>
-      .
-    </footer>
-  );
+	return (
+		<footer className="footer text-right">
+			Managed by&nbsp;
+			<a target="_blank" rel="noopener noreferrer" href="https://rumsan.com">
+				Rumsan
+			</a>
+			.
+		</footer>
+	);
 };
 export default Footer;
