c1ec69a0c3333e1db4ae372c1598774b3e0140e8
Raktim Shrestha <raktim@rumsan.com>
2022-09-22 16:13:27 +0545

    closes #60; Added Google Analytics to track URLs

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import indexRoutes from '../routes';
 import { Router, Route, Switch } from 'react-router-dom';
 import { ToastProvider } from 'react-toast-notifications';
@@ -18,7 +19,17 @@ import SignUp from '../modules/authentication/SignUp';
 import Approval from '../modules/authentication/Approval';
 import { VendorContextProvider } from '../contexts/VendorContext';
 
+const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
+
+ReactGA.initialize(TRACKING_ID);
+
 const App = () => {
+	useEffect(() => {
+		if (TRACKING_ID) {
+			console.log('GAnalytics is enabled...');
+			ReactGA.pageview(window.location.pathname + window.location.search);
+		}
+	}, [TRACKING_ID]);
 	return (
 		<AppContextProvider>
 			<ToastProvider>
@@ -26,20 +37,20 @@ const App = () => {
 					<BeneficiaryContextProvider>
 						<VendorContextProvider>
 							<MobilizerContextProvider>
-							<UserContextProvider>
-								<Router history={History}>
-									<Switch>
-										<Route exact path="/auth/wallet" component={AuthWallet} />
-										<Route exact path="/sign_up" component={SignUp} />
-										<Route exact path="/approval" component={Approval} />
-										<Route path="/passport-control" component={PassportControl} />
-										<Route path="/setup" component={AgencyRegistration} />
-										{indexRoutes.map((prop, key) => {
-											return <PrivateRoute path={prop.path} key={key} component={prop.component} />;
-										})}
-									</Switch>
-								</Router>
-							</UserContextProvider>
+								<UserContextProvider>
+									<Router history={History}>
+										<Switch>
+											<Route exact path="/auth/wallet" component={AuthWallet} />
+											<Route exact path="/sign_up" component={SignUp} />
+											<Route exact path="/approval" component={Approval} />
+											<Route path="/passport-control" component={PassportControl} />
+											<Route path="/setup" component={AgencyRegistration} />
+											{indexRoutes.map((prop, key) => {
+												return <PrivateRoute path={prop.path} key={key} component={prop.component} />;
+											})}
+										</Switch>
+									</Router>
+								</UserContextProvider>
 							</MobilizerContextProvider>
 						</VendorContextProvider>
 					</BeneficiaryContextProvider>
