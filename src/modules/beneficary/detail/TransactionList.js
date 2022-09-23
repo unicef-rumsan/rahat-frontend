0ccdfe128409fc7e82cea4a4710bff100b904871
Santosh <santosh@rumsan.com>
2022-09-24 02:04:14 +0545

    List transactions for beneficiaries Fixes #53

---
+++ src/modules/beneficary/detail/TransactionList.js
@@ -0,0 +1,91 @@
+import React, { useContext, useState, useCallback, useEffect } from 'react';
+import { Card, CardTitle, Table, Row, Col } from 'reactstrap';
+import GrowSpinner from '../../global/GrowSpinner';
+import { AppContext } from '../../../contexts/AppSettingsContext';
+import { listTransactionsByAddress, listBeneficiariesTxs } from '../../../services/ExplorerService';
+import { truncateEthAddress, truncateText } from '../../../utils';
+import moment from 'moment';
+
+const EXPLORER_URL = process.env.REACT_APP_BLOCKCHAIN_EXPLORER;
+const TransactionList = ({ phone }) => {
+	const { appSettings } = useContext(AppContext);
+	const [loading, showLoading] = useState(false);
+	const [transactions, setTransactions] = useState([]);
+
+	const getTxs = useCallback(async () => {
+		const { agency } = appSettings;
+		if (!agency || !agency.contracts) return;
+		showLoading(true);
+		console.log(phone);
+		let data = await listBeneficiariesTxs(agency.contracts.rahat, phone);
+		setTransactions(data);
+		showLoading(false);
+	}, []);
+
+	useEffect(() => {
+		getTxs();
+	}, [getTxs]);
+
+	return (
+		<div className="main">
+			<Card>
+				<div className="stat-card-body" style={{ minHeight: 120 }}>
+					<CardTitle className="title">
+						<Row>
+							<Col md="6">Transaction History</Col>
+							<Col md="6"></Col>
+						</Row>
+					</CardTitle>
+					{loading ? (
+						<GrowSpinner />
+					) : (
+						<Table className="no-wrap v-middle" responsive>
+							<thead>
+								<tr className="border-0">
+									<th className="border-0">Date</th>
+									<th className="border-0">Transaction Hash</th>
+									<th className="border-0">Vendor</th>
+									<th className="border-0">Amount</th>
+								</tr>
+							</thead>
+							<tbody>
+								{transactions.length > 0 ? (
+									transactions.map((tx, i) => {
+										return (
+											<tr key={i}>
+												<td>{moment.unix(tx.timeStamp).format('YYYY-MM-DD HH:mm')}</td>
+												<td>
+													<a
+														href={EXPLORER_URL + '/tx/' + tx.transactionHash}
+														target="_blank"
+														rel="noopener noreferrer"
+													>
+														{truncateText(tx.transactionHash)}
+													</a>
+												</td>
+												<td>
+													<a href={EXPLORER_URL + '/address/' + tx.vendor} target="_blank" rel="noopener noreferrer">
+														{truncateEthAddress(tx.vendor)}
+													</a>
+												</td>
+												<td>{tx.amount}</td>
+											</tr>
+										);
+									})
+								) : (
+									<tr>
+										<td colSpan={2}>No transaction available</td>
+									</tr>
+								)}
+							</tbody>
+						</Table>
+					)}
+				</div>
+			</Card>
+
+			<br />
+		</div>
+	);
+};
+
+export default TransactionList;
--- src/services/ExplorerService.js
+++ src/services/ExplorerService.js
@@ -1,16 +1,49 @@
-import { ethers } from 'ethers';
+import { ethers, utils } from 'ethers';
 import axios from 'axios';
 import CONTRACT from '../constants/contracts';
-import { getContractInstance } from '../blockchain/abi';
+import { getAbi } from '../blockchain/abi';
 
 const EXPLORER_URL = process.env.REACT_APP_BLOCKCHAIN_EXPLORER;
 const keccak256 = txt => ethers.utils.keccak256(ethers.utils.toUtf8Bytes(txt));
-
-const RahatContract = (contractAddress, wallet) => getContractInstance(contractAddress, CONTRACT.RAHAT, wallet);
 
 export async function listTransactionsByAddress(address) {
 	return axios({
 		url: `${EXPLORER_URL}/api?module=account&action=txlist&address=${address}`,
 		method: 'GET'
+	});
+}
+
+export async function listBeneficiariesTxs(contractAddress, phone) {
+	let { data } = await axios.get(`${EXPLORER_URL}/api`, {
+		params: {
+			module: 'logs',
+			action: 'getLogs',
+			fromBlock: 16112707,
+			toBlock: 'latest',
+			address: contractAddress,
+			topic0: utils.id('ClaimAcquiredERC20(address,uint256,uint256)'),
+			topic2: ethers.utils.defaultAbiCoder.encode(['uint256'], [phone]),
+			topic0_2_opr: 'and'
+		}
+	});
+	const iface = new ethers.utils.Interface(getAbi('Rahat'));
+	return data.result.map(d => {
+		const topics = d.topics.filter(d => d !== null);
+		let log = iface.parseLog({
+			data: d.data,
+			topics
+		});
+
+		const { blockNumber, timeStamp, transactionHash } = d;
+		const { vendor, beneficiary, amount } = log.args;
+
+		return {
+			timeStamp: Number(timeStamp),
+			transactionHash,
+			blockNumber,
+			vendor,
+			beneficiary: beneficiary.toNumber(),
+			amount: amount.toNumber()
+		};
 	});
 }
